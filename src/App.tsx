3c6b185d9a77856bf101d6c3256d6fbd2de4fc54
Zhixiang Ye <zhixiangye@Zhixiangs-MBP.fios-router.home>
2025-01-08 12:19:17 -0500

    Confirm the API endpoint and return data format

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://financialmodelingprep.com/api/v3/income-statement/AAPL?period=annual&apikey=jOz8KSdiXywEq5SMlJNREg4BXP7fuA1H'
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, Income Statement Viewer!</h1>
     </div>
   );
 }
